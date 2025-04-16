51547140f78d31c6128b99bfbe6ae21e05e1e119
Roshan1299 <banisettirosh@gmail.com>
2025-04-16 17:36:20 -0600

    Added ProjectIdeasFragment

---
+++ code/app/src/main/java/com/first/projectswipe/ProjectIdeasFragment.kt
@@ -0,0 +1,19 @@
+package com.first.projectswipe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ProjectIdeasFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Temporary layout — replace with your own
+        return inflater.inflate(android.R.layout.simple_list_item_1, container, false)
+    }
+}
