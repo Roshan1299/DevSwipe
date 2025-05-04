ad3e478fc13d225db3ae2c34ae4a5826471cd433
Roshan1299 <banisettirosh@gmail.com>
2025-05-03 19:45:35 -0600

    moved to new package

--- code/app/src/main/java/com/first/projectswipe/home/HomePagerAdapter.kt
+++ code/app/src/main/java/com/first/projectswipe/home/HomePagerAdapter.kt
@@ -1,8 +1,10 @@
-package com.first.projectswipe
+package com.first.projectswipe.home
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.first.projectswipe.projects.ProjectIdeasFragment
+import com.first.projectswipe.SeekingCollaboratorsFragment
 
 class HomePagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int = 2
