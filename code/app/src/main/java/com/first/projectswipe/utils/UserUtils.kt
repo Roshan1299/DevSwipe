f3597500b53888ca41b5fba2226aa6ffb488d9b4
Roshan1299 <banisettirosh@gmail.com>
2025-06-29 17:55:24 -0600

    fix: fun UserInfo

--- code/app/src/main/java/com/first/projectswipe/utils/UserUtils.kt
+++ code/app/src/main/java/com/first/projectswipe/utils/UserUtils.kt
@@ -16,4 +16,21 @@ object UserUtils {
                 callback("Unknown")
             }
     }
+
+    data class UserInfo(val name: String, val profileImageUrl: String?)
+
+    fun getUserInfo(uid: String, callback: (UserInfo) -> Unit) {
+        FirebaseFirestore.getInstance()
+            .collection("users")
+            .document(uid)
+            .get()
+            .addOnSuccessListener { doc ->
+                val name = doc?.getString("name") ?: "Unknown"
+                val profileImageUrl = doc?.getString("profileImageUrl")
+                callback(UserInfo(name, profileImageUrl))
+            }
+            .addOnFailureListener {
+                callback(UserInfo("Unknown", null))
+            }
+    }
 }
