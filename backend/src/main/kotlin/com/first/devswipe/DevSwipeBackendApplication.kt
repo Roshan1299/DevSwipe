9df19087da8afe31235001e0af63e1daf338fd36
Roshan1299 <banisettirosh@gmail.com>
2025-08-10 09:35:14 +0300

    other imp additions for backend

---
+++ backend/src/main/kotlin/com/first/devswipe/DevSwipeBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.first.devswipe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectSwipeBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectSwipeBackendApplication>(*args)
+}
