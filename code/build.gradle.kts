d883add0e2fe841cbbb400df3651da69c0cb3dbc
Roshan1299 <banisettirosh@gmail.com>
2025-08-24 12:23:30 +0300

    feat: added hilt dependencies

--- code/build.gradle.kts
+++ code/build.gradle.kts
@@ -8,5 +8,7 @@ plugins {
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.3.15") // Add this line
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
     }
 }
