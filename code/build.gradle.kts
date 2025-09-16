8c6012f54574df33131660058ecb13d534fe40ca
Roshan1299 <banisettirosh@gmail.com>
2025-09-16 16:07:59 -0600

    fix(Android): Added the Hilt plugin to the project-level build.gradle.kts file.

--- code/build.gradle.kts
+++ code/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.google.services) apply false // Add this line
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
 
 buildscript {
