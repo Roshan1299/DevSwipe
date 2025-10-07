73666b5bd5059e6f29d836ad16dfaa89caa3dce3
Roshan1299 <banisettirosh@gmail.com>
2025-10-07 00:04:33 -0600

    feat(android): migrate UserUtils from Firebase to backend API calls

--- code/app/src/main/java/com/first/projectswipe/utils/UserUtils.kt
+++ code/app/src/main/java/com/first/projectswipe/utils/UserUtils.kt
@@ -1,36 +1,34 @@
-//package com.first.projectswipe.utils
-//
-//import com.google.firebase.firestore.FirebaseFirestore
-//
-//object UserUtils {
-//    fun getUserNameFromUid(uid: String, callback: (String) -> Unit) {
-//        FirebaseFirestore.getInstance()
-//            .collection("users")
-//            .document(uid)
-//            .get()
-//            .addOnSuccessListener { doc ->
-//                val name = doc?.getString("name") ?: "Unknown"
-//                callback(name)
-//            }
-//            .addOnFailureListener {
-//                callback("Unknown")
-//            }
-//    }
-//
-//    data class UserInfo(val name: String, val profileImageUrl: String?)
-//
-//    fun getUserInfo(uid: String, callback: (UserInfo) -> Unit) {
-//        FirebaseFirestore.getInstance()
-//            .collection("users")
-//            .document(uid)
-//            .get()
-//            .addOnSuccessListener { doc ->
-//                val name = doc?.getString("name") ?: "Unknown"
-//                val profileImageUrl = doc?.getString("profileImageUrl")
-//                callback(UserInfo(name, profileImageUrl))
-//            }
-//            .addOnFailureListener {
-//                callback(UserInfo("Unknown", null))
-//            }
-//    }
-//}
+package com.first.projectswipe.utils
+
+import com.first.projectswipe.network.ApiService
+import com.first.projectswipe.network.dto.UserDto
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+object UserUtils {
+
+    data class UserInfo(val name: String, val profileImageUrl: String?)
+
+    fun getUserInfo(uid: String, apiService: ApiService, callback: (UserInfo) -> Unit) {
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                val response = apiService.getUserProfile(uid)
+                if (response.isSuccessful) {
+                    val userProfile = response.body()
+                    if (userProfile != null) {
+                        val userInfo = UserInfo(userProfile.name, userProfile.profileImageUrl)
+                        callback(userInfo)
+                    } else {
+                        callback(UserInfo("Unknown", null))
+                    }
+                } else {
+                    callback(UserInfo("Unknown", null))
+                }
+            } catch (e: Exception) {
+                callback(UserInfo("Unknown", null))
+            }
+        }
+    }
+}
