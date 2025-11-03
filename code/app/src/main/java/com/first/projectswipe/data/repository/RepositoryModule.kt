80739f0339b8e9ae6dcae7fb790a2c4462878048
Roshan1299 <banisettirosh@gmail.com>
2025-11-02 23:54:18 -0700

    feat: Add RepositoryModule for dependency injection

---
+++ code/app/src/main/java/com/first/projectswipe/data/repository/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.first.projectswipe.data.repository
+
+import com.first.projectswipe.domain.repository.ChatRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    
+    @Binds
+    @Singleton
+    abstract fun bindChatRepository(chatRepositoryImpl: ChatRepositoryImpl): ChatRepository
+}
