6034fcee8f48bd45eee8f46f25afedd9d1e97986
Roshan1299 <banisettirosh@gmail.com>
2026-05-06 00:24:48 -0600

    fix: expose BASE_URL as public constant

--- code/app/src/main/java/com/first/projectswipe/network/RetrofitClient.kt
+++ code/app/src/main/java/com/first/projectswipe/network/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://localhost:8080/"
+    const val BASE_URL = "http://localhost:8080/"
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
